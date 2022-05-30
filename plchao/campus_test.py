74544664d451b055315f73611fc77c617b9e1f92
plchao <poseidon31418926.cs06@g2.nctu.edu.tw>
2022-05-30 20:32:39 +0800

    finish test 1, 2

--- plchao/campus_test.py
+++ plchao/campus_test.py
@@ -1,65 +1,91 @@
 from multiprocessing.connection import wait
 import re
+import struct
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-options = webdriver.ChromeOptions()
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_experimental_option('useAutomationExtension', False)
-options.add_experimental_option("prefs", {"profile.password_manager_enabled": False, "credentials_enable_service": False})
 
+import numpy as np
+from io import BytesIO
+from PIL import Image
+
+def png_bytes_to_numpy(png):
+    """Convert png bytes to numpy array
+    
+    Example:  
+    
+    >>> fig = go.Figure(go.Scatter(x=[1], y=[1]))
+    >>> plt.imshow(png_bytes_to_numpy(fig.to_image('png')))
+    """
+    return np.array(Image.open(BytesIO(png)))
+def png_is_equal(png1, png2, threshold=1):
+    mse = ((bfarray - afarray)**2).mean(axis=None)
+    print('mse', mse)
+    return mse < threshold
+
+options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 driver.get("https://smartcampus-1b31f.firebaseapp.com")
 driver.maximize_window()
 driver.implicitly_wait(5)
-enter_as_guest = driver.find_elements(By.CLASS_NAME, value="MuiButtonBase-root")
-# try_enter_as_guest = driver.find_element(By.CSS_SELECTOR, "MuiButtonBase-root.MuiButton-root.MuiButton-text")
-# print(try_enter_as_guest)
-
-# 等 element 出現
-# enter_as_guest = WebDriverWait(driver, 10).until(
-#         EC.presence_of_element_located((By.LINK_TEXT, "以訪客身分進入"))
-#     )
-print(enter_as_guest)
-print(enter_as_guest[0].tag_name)
+enter_as_guest = driver.find_elements(By.CLASS_NAME, value="MuiButton-text")
+# print(enter_as_guest, len(enter_as_guest))
 enter_as_guest[1].click()
-locate = driver.find_elements(By.CLASS_NAME, value="MuiButtonBase-root")
-print(locate, len(locate))
+
+# 等 map 出現
+# print(driver.find_elements(By.ID, "272A6A4D-35F4-4A32-8FBE-A148FACE4046"))
+# enter_as_guest = WebDriverWait(driver, 10).until(
+#     EC.presence_of_element_located((By.ID, "272A6A4D-35F4-4A32-8FBE-A148FACE4046"))
+# )
+time.sleep(5)
+driver.get_screenshot_as_file('./pic/test1_a.png')
+bfclick = driver.get_screenshot_as_png()
+bfarray = png_bytes_to_numpy(bfclick)
+locate = driver.find_elements(By.CLASS_NAME, value="jss64")
 # locate = driver.find_elements(By.CLASS_NAME, value="MuiButtonBase-root MuiFab-root jss64 MuiFab-sizeMedium MuiFab-secondary")
-# driver.execute_script("arguments[0].scrollIntoView();", news)
-WebDriverWait(driver, 10).until(
-        driver.execute_script("return document.readyState")
-    )
-driver.get_screenshot_as_file("before_click.png")
+# print(locate, len(locate))
 locate[0].click()
-driver.get_screenshot_as_file("click.png")
+time.sleep(5)
+driver.get_screenshot_as_file('./pic/test1_b.png')
+afclick = driver.get_screenshot_as_png()
+afarray = png_bytes_to_numpy(afclick)
+driver.quit()
+assert png_is_equal(bfarray, afarray, 1) == False
 
-# news_list = driver.find_elements(By.CLASS_NAME, "su-post")
-# driver.execute_script("arguments[0].scrollIntoView();", news_list[0])
-# news_list[0].click()
-# title = driver.find_element(By.CLASS_NAME, "single-post-title.entry-title")
-# contents = driver.find_elements(By.TAG_NAME, "p")
-# print('title:')
-# print(title.text)
-# print('content:')
-# for paragraph in contents:
-#     print(paragraph.text)
-# print('======')
-# target = '310552011'
-# driver.switch_to.new_window('tab')
-# driver.get("https://www.google.com")
-# assert len(driver.window_handles) == 2
-# search_box = driver.find_element(By.CLASS_NAME, "gLFyf.gsfi")
-# search_box.send_keys(target+'\n')
-# search_result_list = driver.find_elements(By.CLASS_NAME, "LC20lb.MBeuO.DKV0Md")
-# print('second result of ' + target)
-# print(search_result_list[1].text)
+options = webdriver.ChromeOptions()
+# close geolocation
+options.add_experimental_option("prefs", {"profile.default_content_setting_values.geolocation" :2})
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-# # with open('tmp.txt', 'w', encoding='UTF-8') as f:
-# #     f.write(driver.page_source)
+driver.get("https://smartcampus-1b31f.firebaseapp.com")
+driver.maximize_window()
+driver.implicitly_wait(5)
+enter_as_guest = driver.find_elements(By.CLASS_NAME, value="MuiButton-text")
+# print(enter_as_guest, len(enter_as_guest))
+enter_as_guest[1].click()
 
-# driver.quit()
+# 等 map 出現
+# print(driver.find_elements(By.ID, "272A6A4D-35F4-4A32-8FBE-A148FACE4046"))
+# enter_as_guest = WebDriverWait(driver, 10).until(
+#     EC.presence_of_element_located((By.ID, "272A6A4D-35F4-4A32-8FBE-A148FACE4046"))
+# )
+time.sleep(5)
+driver.get_screenshot_as_file('./pic/test2_a.png')
+bfclick = driver.get_screenshot_as_png()
+bfarray = png_bytes_to_numpy(bfclick)
+locate = driver.find_elements(By.CLASS_NAME, value="jss64")
+# locate = driver.find_elements(By.CLASS_NAME, value="MuiButtonBase-root MuiFab-root jss64 MuiFab-sizeMedium MuiFab-secondary")
+# print(locate, len(locate))
+locate[0].click()
+time.sleep(5)
+driver.get_screenshot_as_file('./pic/test2_b.png')
+afclick = driver.get_screenshot_as_png()
+afarray = png_bytes_to_numpy(afclick)
+driver.quit()
+assert png_is_equal(bfarray, afarray, 1)
+
